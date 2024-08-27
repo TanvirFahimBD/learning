e43857d9c1b0a59940b44b58474635064edcde3d
TanvirFahimBD <tanvirfahim6939@gmail.com>
2024-08-27 23:57:26 +0600

    ts primitive types

--- mern-l2-ph/ms-1-ts/m2-basic-types-ts/2.primitive-type.ts
+++ mern-l2-ph/ms-1-ts/m2-basic-types-ts/2.primitive-type.ts
@@ -1,35 +1,36 @@
-/**
+{/**
  * primitive type: string, number, boolean, null, undefined
  */
 
 /**
  * string type: only string methods are available for this variable
  */
-// const course: string = "Next level web Dev";
-// console.log(course);
+const course: string = "Next level web Dev";
+console.log(course);
 
 /**
  * number type: only number methods are available for this variable
  */
-// const rollNumber: number = 23;
-// console.log(rollNumber);
+const rollNumber: number = 23;
+console.log(rollNumber);
 
 /**
  *  boolean type: true false
  */
-// const isAdmin: boolean = true;
-// console.log(isAdmin);
+const isAdmin: boolean = true;
+console.log(isAdmin);
 
 /**
  * any type: if not declare explicitly type. then it become any type
  */
-// let course2;
-// course2 = 23;
-// course2 = "fahim";
-// console.log(course2);
+let course2;
+course2 = 23;
+course2 = "fahim";
+console.log(course2);
 
 /**
  * undefined type: const & assign undefine
  */
-// const course3 = undefined;
-// console.log(course3);
+const course3 = undefined;
+console.log(course3);
+}
