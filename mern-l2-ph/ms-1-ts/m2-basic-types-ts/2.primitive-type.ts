19eac78a713c3fc43f7c37e2bdf7027e6ad87a1b
TanvirFahimBD <tanvirfahim6939@gmail.com>
2024-08-28 22:21:03 +0600

    ts implcit & explicit types

--- mern-l2-ph/ms-1-ts/m2-basic-types-ts/2.primitive-type.ts
+++ mern-l2-ph/ms-1-ts/m2-basic-types-ts/2.primitive-type.ts
@@ -1,5 +1,6 @@
 {/**
  * primitive type: string, number, boolean, null, undefined
+ * type 2 : implicit(own decision by ts), explicit(defined)
  */
 
 /**
@@ -29,8 +30,14 @@ course2 = "fahim";
 console.log(course2);
 
 /**
+ * null type: const & assign null
+ */
+const course4 : null= null;
+console.log(course4);
+
+/**
  * undefined type: const & assign undefine
  */
-const course3 = undefined;
+const course3:undefined = undefined;
 console.log(course3);
 }
