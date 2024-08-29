ccb2f16b9165232afd0b51d9dfa6d105f3b6f4d6
TanvirFahimBD <tanvirfahim6939@gmail.com>
2024-08-29 09:08:59 +0600

    ts array tuple

--- mern-l2-ph/ms-1-ts/m2-basic-types-ts/3.array.ts
+++ mern-l2-ph/ms-1-ts/m2-basic-types-ts/3.array.ts
@@ -1,29 +1,30 @@
-//! array type
+{
+//? array & tuple
 /**
- * !string of array
+ * * string of array
  */
-// const names: string[] = ["fahim", "tanvir"];
-// console.log(names);
-// names.map((name) => console.log(name.toUpperCase()));
+const names: string[] = ["fahim", "tanvir"];
+console.log(names);
+names.map((name) => console.log(name.toUpperCase()));   //suggest name methods on each element of map
 
 /**
- * !number of array
+ * * number of array
  */
-// const rollNumbers1: number[] = [34, 45];
-// rollNumbers1[0] = 455;
-// console.log(rollNumbers1);
-// rollNumbers1.map((rollNum) => console.log(rollNum.toString()));
+const rollNumbers1: number[] = [34, 45];
+rollNumbers1[0] = 455;
+console.log(rollNumbers1);
+rollNumbers1.map((rollNum) => console.log(rollNum.toString())); //suggest string methods on each element of map
 
 /**
- * !union type
+ * * union type - misalinus types values into array & those types are pushable
  */
-// const names = ["abul", "kabul", 43, 54, false];
-// names.push(3);
-// console.log(names);
+const names2 = ["abul", "kabul", 43, 54, false];
+names2.push(3);
+console.log(names);
 
 /**
- * !tuple: strictly declare type not type changeable
- * during key value use-case apply tuple
+ * * tuple: strictly declare type not type changeable - during key value use-case apply tuple
  */
-// const user: [number, string] = [45, "fahim"];
-// console.log(user);
+const user: [number, string] = [45, "fahim"]; //if not declared then one index number value can be replace by string so there is need of tuple
+console.log(user);
+}
