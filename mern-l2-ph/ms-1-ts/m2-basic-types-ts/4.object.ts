a4db609e6df5c6f1dc2e30963f3dccb979bb8826
TanvirFahimBD <tanvirfahim6939@gmail.com>
2024-08-30 07:17:53 +0600

    ts object, optional type, literal type & access modifier readonly added

--- mern-l2-ph/ms-1-ts/m2-basic-types-ts/4.object.ts
+++ mern-l2-ph/ms-1-ts/m2-basic-types-ts/4.object.ts
@@ -1,55 +1,64 @@
-//! 2-4: Object, Literal Types and Optional types
+{
+
+//? 2-4: Object, Literal Types and Optional types
+    
 /**
- * !optional type: variable?: type
+ * * optional type: variable?: type
  */
-// const user2: {
-//   company: string;
-//   name: string;
-//   age: number;
-//   isMarried: boolean;
-//   wife: string;
-// } = {
-//   company: "PH",
-//   name: "fahim",
-//   age: 34,
-//   isMarried: true,
-//   wife: "xyz",
-// };
-// console.log(user2);
+const user: {
+  company: string;
+  name: string;
+  age: number;
+  isMarried: boolean;
+  wife?: string;
+} = {
+  company: "PH",
+  name: "fahim",
+  age: 34,
+  isMarried: true,
+};
+
+console.log(user);
 
 /**
- * !literal type: value as type. type not changeable
+ * * literal type: value as type. type/value not changeable: same value need to give
  */
-// const user2: {
-//   company: "ph";
-//   name: string;
-//   age: number;
-//   isMarried: boolean;
-//   wife: string;
-// } = {
-//   company: "ph",
-//   name: "fahim",
-//   age: 34,
-//   isMarried: true,
-//   wife: "xyz",
-// };
-// ! user2.company = 'ph2'
+const user2: {
+  company: "ph";
+  name: string;
+  age: number;
+  isMarried: boolean;
+  wife: string;
+} = {
+  company: "ph",
+  name: "fahim",
+  age: 34,
+  isMarried: true,
+  wife: "xyz",
+};
 
-/**
- * !access modifier: readonly. only readable not changeable
- */
-// const user2: {
-//     readonly company: string;
-//     name: string;
-//     age: number;
-//     isMarried: boolean;
-//     wife: string;
-// } = {
-//     company: 'ph',
-//     name: 'fahim',
-//     age: 34,
-//     isMarried: true,
-//     wife: 'xyz'
-// }
 // user2.company = 'ph2'
-// console.log(user2);
+console.log(user2);
+        
+/**
+ * * access modifier: readonly. only readable not changeable. READONLY  and literal type same in one way that both property can't be changed later
+ */
+    
+const user3: {
+    readonly company: string;
+    name: string;
+    age: number;
+    isMarried: boolean;
+    wife: string; 
+} = {
+    company: 'ph2',
+    name: 'fahim',
+    age: 34,
+    isMarried: true,
+    wife: 'xyz'
+}
+    
+// user3.company = 'ph2';
+console.log(user3);
+    
+}
