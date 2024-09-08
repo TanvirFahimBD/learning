f02c599df6d53b9dd0a4b4fd021b0620ad5442e8
TanvirFahimBD <tanvirfahim6939@gmail.com>
2024-09-08 22:01:01 +0600

    Type assersion in ts

--- mern-l2-ph/ms-1-ts/m3-advance-types-ts/1.type-assertion.ts
+++ mern-l2-ph/ms-1-ts/m3-advance-types-ts/1.type-assertion.ts
@@ -1,39 +1,43 @@
-//! 3-1: Type Assertion
+{
+//* 3-1: Type Assertion
 /**
  * ? Type Assertion: when confirm the return type then assert the type before on own. as string or <string> is syntax convention. <type> this syntax not work in tsx file work on others. so as syntax is safe to use everywhere.
  */
-// let diffType: any;
-// diffType = "hello ts";
-// diffType = 343;
+let diffType: any;
+diffType = "hello ts";
+diffType = 343;
 // console.log((diffType as string).length);
-// console.log((diffType as number).toFixed());
+console.log((diffType as number).toFixed());
 
 /**
- * assert type as or <>
+ * ? assert type as or <>
  */
-// function kgToGram(kgVal: string | number): string | number | undefined {
-//   if (typeof kgVal === "string") {
-//     return `Total ${parseInt(kgVal) * 1000} gm`;
-//   }
-//   if (typeof kgVal === "number") {
-//     return kgVal * 1000;
-//   }
-// }
+function kgToGram(kgVal: string | number): string | number | undefined {
+  if (typeof kgVal === "string") {
+    return `Total ${parseInt(kgVal) * 1000} gm`;
+  }
+  if (typeof kgVal === "number") {
+    return kgVal * 1000;
+  }
+}
 // console.log(kgToGram(5) as number);
 // console.log(kgToGram("54") as string);
 
-// console.log(<number>kgToGram(5));
-// console.log(<string>kgToGram("54"));
+console.log(<number>kgToGram(5));
+console.log(<string>kgToGram("54"));
 
 /**
- * custom type assertion
+ * ? custom type assertion
  */
-// type CustomErrorType = {
-//   message: string;
-// };
-// try {
-//   throw new Error("Error going on ......");
-// } catch (e) {
-// console.log((e as CustomErrorType).message);
-// console.log((<CustomErrorType>e).message);
-// }
+type CustomErrorType = {
+  message: string;
+};
+
+try {
+  throw new Error("Error going on...");
+}
+catch (e) {
+    console.log((e as CustomErrorType).message);
+}
+
+}
