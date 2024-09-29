a6ac6f3b8a6c0542e5195911a96171e8911bd2c7
TanvirFahimBD <tanvirfahim6939@gmail.com>
2024-09-29 22:47:54 +0600

    learning ongoing

---
+++ phitron/m-1/firstCode.c
@@ -0,0 +1,19 @@
+// output write
+//  #include <stdio.h>
+
+// int main()
+// {
+//     printf("Hello World2!");
+//     return 0;
+// }
+
+// input take & output write
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    printf("a = %d", a);
+    return 0;
+}
---
+++ phitron/m-2/m-2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    // * arithmatic operations
+    //  int a, b;
+    //  scanf("%d", &a);
+    //  scanf("%d", &b);
+    //  printf("%d + %d = %d \n", a, b, a + b);
+    //  printf("%d - %d = %d \n", a, b, a - b);
+    //  printf("%d * %d = %d \n", a, b, a * b);
+    //  printf("%d / %d = %d \n", a, b, a / b);
+    //  printf("%d %% %d = %d \n", a, b, a % b);
+
+    // exceptional cases
+    // int a = 100000;
+    // double b = 100000;
+    // long long result = a * b;
+    // printf("%lld", result);
+
+    // int a = 100000, b = 100000;
+    // long long result = 1ll * a * b;
+    // printf("%lld", result);
+
+    // printf("%d\n", 5 / 3);
+    // printf("%f\n", 5 / 3.0);
+    // printf("%d\n", 0 / 5);
+
+    // create error
+    //  printf("%d", 5 / 0);
+
+    //* assignment operator
+    // int a = 10;
+    // printf("%d\n", a++);
+    // printf("%d\n", a);
+    // printf("%d\n", ++a);
+
+    return 0;
+}
