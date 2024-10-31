82e920dd0ca2b9a7e1e902eb3bcfcb8239ed0a87
TanvirFahimBD <tanvirfahim6939@gmail.com>
2024-11-01 05:18:26 +0600

    cp learning ongoing

--- phitron/week4-ferquency-array-2d-array-and-pointer/m15_5-week4-practice-day2/m15.c
+++ phitron/week4-ferquency-array-2d-array-and-pointer/m15_5-week4-practice-day2/m15.c
@@ -6,11 +6,24 @@ int main()
 {
     //* A. Add
     //* https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/A
+    // int x, y;
+    // scanf("%d %d", &x, &y);
+    // int *a = &x;
+    // int *b = &y;
+    // printf("%d\n", *a + *b);
+
+    // return 0;
+
+    //* E. Swap
+    //* https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/E
     int x, y;
     scanf("%d %d", &x, &y);
     int *a = &x;
     int *b = &y;
-    printf("%d\n", *a + *b);
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    printf("%d %d\n", *a, *b);
 
     return 0;
 }
