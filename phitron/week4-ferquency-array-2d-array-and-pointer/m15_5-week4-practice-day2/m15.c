b71670d347a2ecd049bce35b600aaf5e2c1b736d
TanvirFahimBD <tanvirfahim6939@gmail.com>
2024-11-02 20:38:01 +0600

    cp learning ongoing

--- phitron/week4-ferquency-array-2d-array-and-pointer/m15_5-week4-practice-day2/m15.c
+++ phitron/week4-ferquency-array-2d-array-and-pointer/m15_5-week4-practice-day2/m15.c
@@ -16,14 +16,118 @@ int main()
 
     //* E. Swap
     //* https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/E
-    int x, y;
-    scanf("%d %d", &x, &y);
-    int *a = &x;
-    int *b = &y;
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-    printf("%d %d\n", *a, *b);
+    // int x, y;
+    // scanf("%d %d", &x, &y);
+    // int *a = &x;
+    // int *b = &y;
+    // int temp = *a;
+    // *a = *b;
+    // *b = temp;
+    // printf("%d %d\n", *a, *b);
+
+    // return 0;
+
+    //* K. Max and Min
+    //* https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/K
+    // int x, y, z;
+    // scanf("%d %d %d", &x, &y, &z);
+    // int *a = &x;
+    // int *b = &y;
+    // int *c = &z;
+
+    // if (*a <= *b && *a <= *c)
+    // {
+    //     printf("%d ", *a);
+    // }
+    // else if (*b <= *a && *b <= *c)
+    // {
+    //     printf("%d ", *b);
+    // }
+    // else
+    // {
+    //     printf("%d ", *c);
+    // }
+
+    // if (*a >= *b && *a >= *c)
+    // {
+    //     printf("%d", *a);
+    // }
+    // else if (*b >= *a && *b >= *c)
+    // {
+    //     printf("%d", *b);
+    // }
+    // else
+    // {
+    //     printf("%d", *c);
+    // }
+
+    // return 0;
+
+    //* A. Summation
+    //* https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/A
+    // int n;
+    // long long int sum = 0;
+    // scanf("%d", &n);
+    // int ar[n];
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     scanf("%d", &ar[i]);
+    // }
+    // int *ptr = ar;
+
+    // while (ptr < ar + n)
+    // {
+    //     sum += *ptr;
+    //     ptr++;
+    // }
+
+    // if (sum < 0)
+    // {
+    //     printf("%lld", sum * (-1));
+    // }
+    // else
+    // {
+    //     printf("%lld", sum);
+    // }
+
+    // return 0;
+
+    //* C. Replacement
+    //* https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/C
+    int n;
+    long long int sum = 0;
+    scanf("%d", &n);
+    int ar[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ar[i]);
+    }
+    int *ptr = ar;
+
+    while (ptr < ar + n)
+    {
+
+        if (*ptr < 0)
+        {
+            *ptr = 2;
+        }
+        else if (*ptr == 0)
+        {
+            *ptr = 0;
+        }
+        else
+        {
+            *ptr = 1;
+        }
+        ptr++;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", ar[i]);
+    }
 
     return 0;
 }
